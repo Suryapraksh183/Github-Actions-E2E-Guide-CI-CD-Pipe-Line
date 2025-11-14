29580d6f3aa518a99fbf6936a0e719ea117ef59c
SuriyaTestAutomation <98521294+Suryapraksh183@users.noreply.github.com>
2025-11-14 18:25:52 +0530

    Delete src/test/java/org/google/home/utils/ConstantVariables.java

--- src/test/java/org/google/home/utils/ConstantVariables.java
+++
@@ -1,9 +0,0 @@
-package org.google.home.utils;
-
-import java.io.File;
-
-public class ConstantVariables {
-
-	public static final String btnTxtDonateMyself = "DONATE AS MYSELF";
-	public static final String fs = File.separator;
-}
